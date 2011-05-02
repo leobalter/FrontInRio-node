6c7fa4ce4fc513af41e6835bdbfcff7548152e4c
Leo Balter <leonardo.balter@gmail.com>
2011-05-02 00:01:08 -0300

    google maps na home e link para call4paperz

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,32 @@
-$(function(){
-	$("#jquery-test").html("jQuery is loaded");
-});
+jQuery(function($) { 
+    var myLatlng = new google.maps.LatLng(-22.95369, -43.16880);
+    var myOptions = {
+      zoom: 16,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    map.setCenter(myLatlng);
+    
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'FrontInRio 2011'
+    });
+
+    var contentString = '<div style="height:50px"><h1><a href="http://www.frontinrio.com.br">FrontInRio 2011</a></h1>\n';
+    contentString += '<p><strong>Local:</strong> Av. Pasteur 458, prédio do CCET, sala 204, no bairro da Urca, Rio de Janeiro</p></div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        position: myLatlng,
+        minHeight: 150
+        
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+    });
+     
+});
+
